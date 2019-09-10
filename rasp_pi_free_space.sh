33f3258e3dfc61ec28bf3e77aaabb135814df2e9
s4lat <zakazchikm@gmail.com>
2019-09-10 18:58:04 +0500

    kek

---
+++ rasp_pi_free_space.sh
@@ -0,0 +1,3 @@
+sudo apt-get purge wolfram-engine libreoffice* scratch -y
+sudo apt-get clean
+sudo apt autoremove -y
---
+++ rasp_pi_setup.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libatlas3-base
+
+
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
+
+python3 -m pip install dlib
+
+sudo sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
+
+python3 -m pip install -r requirements.txt
