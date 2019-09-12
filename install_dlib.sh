6df76c2aa69afbf06287c0d4e53610829cce7a50
s4lat <zakazchikm@gmail.com>
2019-09-12 10:02:46 +0500

    divide

---
+++ install_dlib.sh
@@ -0,0 +1,7 @@
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
+
+python3 -m pip install dlib
+
+sudo sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
---
+++ install_opencv.sh
@@ -0,0 +1,4 @@
+wget -O https://github.com/opencv/opencv/archive/4.1.1.zip
+unzip opencv.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.1.1.zip
+unzip opencv_contrib.zip
