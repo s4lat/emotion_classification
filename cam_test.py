410a58d61741e0f12cb49b7ea5056f46842fcda9
s4lat <zakazchikm@gmail.com>
2019-09-12 17:03:16 +0500

    cam_test.py, install scripts

--- cam_test.py
+++ cam_test.py
@@ -1,15 +1,40 @@
 from imutils.video import VideoStream
-import cv2
+from imutils.video import FPS
+import numpy as np
+import cv2, imutils
+
+WIDTH, HEIGHT = 640, 360
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+fontScale              = 0.5
+fontColor              = (180, 0, 150)
+lineType               = 2
 
 
-cap = VideoStream(src=0)
 cv2.namedWindow('cam')
+
+cap = VideoStream(src=0, resolution=(WIDTH, HEIGHT)).start()
+fps = FPS().start()
+
+n_frames = 0
 
 while True:
 	frame = cap.read()
-	cv2.resize(frame, (640, 360))
+	frame = cv2.resize(frame, (WIDTH, HEIGHT))
+
+	fps.update()
+	fps.stop()
+
+	cv2.putText(frame, 'FPS: %.2f' % fps.fps(),
+					(20, 20),
+					font,
+					fontScale,
+					fontColor,
+					lineType)
 
 	cv2.imshow('cam', frame)
+
+
 
 	k = cv2.waitKey(1)
 
