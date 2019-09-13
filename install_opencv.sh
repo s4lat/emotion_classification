93a19d2eb10add05ca69511e3bf5d985c341fed9
s4lat <zakazchikm@gmail.com>
2019-09-13 07:28:20 +0500

    opencv install changes

--- install_opencv.sh
+++ install_opencv.sh
@@ -23,9 +23,18 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D INSTALL_PYTHON_EXAMPLES=OFF \
     -D OPENCV_ENABLE_NONFREE=ON \
     -D CMAKE_SHARED_LINKER_FLAGS='-latomic' \
-    -D BUILD_EXAMPLES=OFF ..
+    -D BUILD_EXAMPLES=OFF .. \
+    -D PYTHON3_EXECUTABLE=$(which python3) \
+    -D PYTHON_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+  	-D PYTHON_INCLUDE_DIR2=$(python3 -c "from os.path import dirname; from distutils.sysconfig import get_config_h_filename; print(dirname(get_config_h_filename()))") \
+  	-D PYTHON_LIBRARY=$(python3 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))") \
+  	-D PYTHON3_NUMPY_INCLUDE_DIRS=$(python3 -c "import numpy; print(numpy.get_include())") \
+  	-D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
 
 make -j1
 
 sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
 /etc/init.d/dphys-swapfile restart
+
+make install
+ldconfig
