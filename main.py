ac83e6bc58424144bea254dcf02eb841479fa441
s4lat <zakazchikm@gmail.com>
2019-09-14 00:17:51 +0500

    Some beauty changes, SCALE_FACTOR = 1.5

--- main.py
+++ main.py
@@ -15,19 +15,22 @@ parser.add_argument('--model', default="mini_x",
 args = parser.parse_args()
 model = args.model
 
+SCALE_FACTOR = 1.5
+
 OUT_WIDTH, OUT_HEIGHT = 640, 360
 
-IN_WIDTH, IN_HEIGHT = 320, 180
+IN_WIDTH, IN_HEIGHT = OUT_WIDTH // SCALE_FACTOR, OUT_HEIGHT // SCALE_FACTOR
+IN_WIDTH, IN_HEIGHT = int(IN_WIDTH), int(IN_HEIGHT)
 
-SCALE_FACTOR = 2
 
 NOT_SELECTED_COLOR = (0, 200, 0)[::-1] #RGB to BGR
 SELECTED_COLOR = (255, 140, 0)[::-1]
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 fontScale              = 0.5
-fontColor              = (0, 255, 0)[::-1]
-lineType               = 2
+fontColor              = (255, 255, 255)[::-1]
+bgColor                = (255, 0, 0)[::-1]
+lineType               = cv2.LINE_AA
 
 EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
  "neutral"]
@@ -67,24 +70,40 @@ def draw_border(img, pt1, pt2, color, thickness, r, d):
     x2,y2 = pt2
 
     # Top left
-    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
-    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
-    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness, cv2.LINE_AA)
+    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness, cv2.LINE_AA)
+    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness, cv2.LINE_AA)
 
     # Top right
-    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
-    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
-    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness, cv2.LINE_AA)
+    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness, cv2.LINE_AA)
+    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness, cv2.LINE_AA)
 
     # Bottom left
-    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
-    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
-    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness, cv2.LINE_AA)
+    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness, cv2.LINE_AA)
+    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness, cv2.LINE_AA)
 
     # Bottom right
-    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
-    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
-    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness, cv2.LINE_AA)
+    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness, cv2.LINE_AA)
+    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness, cv2.LINE_AA)
+
+def draw_text_w_background(img, text, coords, font, fontScale, fontColor, bgColor, thickness):
+	lines = text.split("\n")
+	(x, y) = coords
+
+	for i, line in enumerate(lines):
+		(w, h) = cv2.getTextSize(line, font, fontScale=fontScale, thickness=thickness)[0]
+		y = coords[1] + h*i
+		cv2.rectangle(img, (x-5, y-h), (x+w+5, y+5), bgColor, cv2.FILLED)
+
+	h += 2
+	for i, line in enumerate(lines):
+		y = coords[1] + h*i
+		cv2.putText(img, line, (x, y), font, fontScale, fontColor, thickness, cv2.LINE_AA)
+
+
 
 
 cv2.namedWindow('cam')
@@ -121,62 +140,34 @@ while True:
 			emotion = np.argmax(preds)
 
 			x, y, w, h = (x*SCALE_FACTOR, y*SCALE_FACTOR, w*SCALE_FACTOR, h*SCALE_FACTOR)
+			x, y, w, h = (int(x), int(y), int(w), int(h))
 
 			draw_border(out_frame, (x, y), (x+w, y+h), SELECTED_COLOR, 2, 5, 10)
-			cv2.putText(out_frame, EMOTIONS[emotion] + "_%.f" % (preds[emotion]*100),
+			draw_text_w_background(out_frame, EMOTIONS[emotion] + "_%.f" % (preds[emotion]*100),
 					(x+w+10, y+h+10),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
-			cv2.putText(out_frame, "Press <C> on your keyboard",
+					font, fontScale,
+					fontColor, bgColor, 1)
+			draw_text_w_background(out_frame, "Press <C> on your keyboard\nif you want to choose other face",
 					(10, 15),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
-			cv2.putText(out_frame, "if you want to choose other face",
-				(10, 30),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
+					font, fontScale,
+					fontColor, bgColor, 1)
 		else:
 			tracker_initiated = False
 
 	else:
 		faces = face_detector.detectMultiScale(gray)
 		for x, y, w, h in faces:
-			roi = gray[y:y+h, x:x+w]
-			roi = cv2.resize(roi, input_shape, interpolation=cv2.INTER_AREA)
-			roi = roi.astype("float") / 255.0
-			roi = img_to_array(roi)
-			roi = np.expand_dims(roi, axis=0)
-			preds = emotion_classifier.predict(roi)[0]
-			emotion = np.argmax(preds)
-
 			x, y, w, h = (x*SCALE_FACTOR, y*SCALE_FACTOR, w*SCALE_FACTOR, h*SCALE_FACTOR)
+			x, y, w, h = (int(x), int(y), int(w), int(h))
 
-			draw_border(out_frame, (x, y), (x+w, y+h), SELECTED_COLOR, 2, 5, 10)
-			cv2.putText(out_frame, EMOTIONS[emotion] + "_%.f" % (preds[emotion]*100),
-					(x+w+10, y+h+10),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
-
-		# faces = face_locations(gray)
-		# for y0, x1, y1, x0 in faces:
-		# 	draw_border(frame, (x0, y0), (x1, y1), NOT_SELECTED_COLOR, 2, 5, 10)
+			draw_border(out_frame, (x, y), (x+w, y+h), NOT_SELECTED_COLOR, 2, 5, 10)
 
 		cv2.setMouseCallback('cam', choose_face, faces)
 
-		cv2.putText(out_frame, "Click on face you want to track",
+		draw_text_w_background(out_frame, "Click on face you want to track",
 					(10, 15),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
+					font, fontScale,
+					fontColor, bgColor, 1)
 
 	#Calculate fps
 	fps.update()
@@ -190,12 +181,10 @@ while True:
 		frame_ind = 0
 
 	#Draw fps
-	cv2.putText(out_frame, 'FPS: %.2f' % CURRENT_FPS,
+	draw_text_w_background(out_frame, 'FPS: %.2f' % CURRENT_FPS,
 					(20, IN_HEIGHT-20),
-					font,
-					fontScale,
-					fontColor,
-					lineType)
+					font, fontScale,
+					fontColor, bgColor, 1)
 
 	cv2.imshow('cam', out_frame)
 
