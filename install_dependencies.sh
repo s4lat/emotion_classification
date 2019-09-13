b1d39ed3b160c346598095be9257790f1b1e0eec
s4lat <zakazchikm@gmail.com>
2019-09-13 19:48:24 +0500

    dlib with NEON, new dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,7 +7,7 @@ apt-get install -y libhdf5-dev libhdf5-serial-dev
 apt-get install -y libqtgui4 libqtgui4-test
 apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-apt-get install -y libxvidcore-dev libx264-dev
+apt-get install -y libxvidcore-dev libx264-dev libx11-dev
 apt-get install -y libcanberra-gtk*
-apt-get install -y libatlas-base-dev gfortran
+apt-get install -y libatlas-base-dev libopenblas-dev liblapack-dev gfortran
 apt-get install -y python2.7-dev python3-dev python3-pip
--- install_dlib.sh
+++ install_dlib.sh
@@ -3,7 +3,9 @@ python3 -m pip install wheel
 sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 /etc/init.d/dphys-swapfile restart
 
-python3 -m pip install dlib
+git clone https://github.com/davisking/dlib.git
+cd dlib
+python3 setup.py install --yes USE_NEON_INSTRUCTIONS
 
 sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
 /etc/init.d/dphys-swapfile restart
