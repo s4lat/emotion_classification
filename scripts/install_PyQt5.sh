36f46d7524fb0e03635abf08380c6746b905c193
s4lat <zakazchikm@gmail.com>
2019-09-14 17:42:27 +0500

    Install pyqt5 fix

--- scripts/install_PyQt5.sh
+++ scripts/install_PyQt5.sh
@@ -2,7 +2,7 @@ wget -O pyqt5.tar.gz https://www.riverbankcomputing.com/static/Downloads/PyQt5/5
 tar -xf pyqt5.tar.gz
 
 rm -rf pyqt5.tar.gz
-cd PyQt5_gpl-5
+cd PyQt5_gpl-5.13.1
 
 python3 configure.py
 
