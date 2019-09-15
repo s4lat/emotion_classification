132f6a6dffd35581709b0414631150a71559ba77
s4lat <zakazchikm@gmail.com>
2019-09-15 23:41:10 +0500

    PREFFERED_THEME='cleanlooks'

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 from main import MainWindow
-from PyQt5.QtWidgets import QApplication
-import sys
+from PyQt5.QtWidgets import QApplication, QStyleFactory
+import sys, cfg
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
+	
+	if cfg.PREFFERED_THEME in QStyleFactory.keys():
+		app.setStyle(cfg.PREFFERED_THEME)
+
 	win = MainWindow()
 	win.show()
 	sys.exit(app.exec_())
