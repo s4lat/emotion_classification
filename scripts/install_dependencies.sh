ad99f3cd97205aae72b0fca780826153e5eb63c3
s4lat <zakazchikm@gmail.com>
2019-09-15 15:53:37 +0500

    Dependencies fix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,5 +11,5 @@ apt-get install -y libxvidcore-dev libx264-dev libx11-dev
 apt-get install -y libcanberra-gtk*
 apt-get install -y libatlas-base-dev libopenblas-dev liblapack-dev gfortran
 apt-get install -y python2.7-dev python3-dev python3-pip
-apt-get install -y qt5-default, pyqt5-dev, pyqt5-dev-tools
+apt-get install -y qt5-default pyqt5-dev pyqt5-dev-tools
 python3 -m pip install wheel
